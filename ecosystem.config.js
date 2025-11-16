9921145c332502b0a8e9df0f091eb12efb567590
yab21 <yablaiyablairubenvirgil@gmail.com>
2025-11-16 17:05:48 +0000

    amelioration de la page projet

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,7 +5,7 @@ module.exports = {
       script: "node_modules/next/dist/bin/next",
       args: "start",
       exec_mode: "cluster",
-      instances: 2,
+      instances: 1,
       env: {
         NODE_ENV: "production",
         PORT: 3000
@@ -15,16 +15,16 @@ module.exports = {
         PORT: 3000
       },
       watch: false,
-      max_memory_restart: "512M",
-      restart_delay: 3000,
+      max_memory_restart: "1G",
+      restart_delay: 5000,
       log_file: "./logs/combined.log",
       out_file: "./logs/out.log",
       error_file: "./logs/error.log",
       log_date_format: "YYYY-MM-DD HH:mm:ss Z",
-      min_uptime: "5s",
+      min_uptime: "10s",
       max_restarts: 15,
       autorestart: true,
-      node_args: "--max_old_space_size=512",
+      node_args: "--max_old_space_size=1024",
       env_file: ".env"
     }
   ]
